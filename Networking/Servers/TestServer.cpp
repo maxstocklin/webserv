863d9f1a13e244c3d9bd64ffae8653c31f4fa7ad
Maxime <matchmatchandmatch@gmail.com>
2023-09-05 09:24:28 +0200

    commentented testserver.cpp

--- Networking/Servers/TestServer.cpp
+++ Networking/Servers/TestServer.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   TestServer.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mstockli <mstockli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: max <max@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 15:29:07 by mstockli          #+#    #+#             */
-/*   Updated: 2023/09/04 21:02:17 by mstockli         ###   ########.fr       */
+/*   Updated: 2023/09/05 01:22:10 by max              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,23 @@ void TestServer::accepter()
 	new_socket = accept(get_socket()->get_sock(), (struct sockaddr *)&address, (socklen_t *)&addrlen);
 	
 	memset(buffer, 0, sizeof(buffer)); // Zero out the buffer
+	/*
+	1. Ensure that the server handles partial reads and writes if the data is large for one call
+	--> getnextline()
+	2. check for errors (i.e. if bytes_read = -1)
+	*/
 	int bytes_read = read(new_socket, buffer, sizeof(buffer) - 1);
 	buffer[bytes_read] = '\0'; // Null-terminate the buffer
 }
 
+// for now, it doesn't receive anything, this function will be used to parse the HTTP request
+// error handling: invalid data in the HTTP request
+// handle CGI scripts?
 void TestServer::handler()
 {
+	std::cout << "THIS IS THE BUFFER BIATCH" << std::endl;
 	std::cout << buffer << std::endl;
+	std::cout << "THIS IS THE BUFFER BIATCH22" << std::endl;
 }
 
 void TestServer::responder()
@@ -65,7 +75,14 @@ void TestServer::responder()
 	char response[2048]; // Make sure the size is enough to contain both headers and content
 	sprintf(response, "%s%s", response_headers, html_content);
 
+	// later, this function will, instead of 'response,' use the parsed buffer from the read function
 	write(new_socket, response, strlen(response));
+	
+	/*
+	AFTER THE PARSING:
+	Depending on the HTTP headers (like Connection: keep-alive), 
+	We might keep the connection open for further requests from the same client.
+	*/
 	close(new_socket);
 }
 
