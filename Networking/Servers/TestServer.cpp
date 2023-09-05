5fc789992f92735733cd07f5896b9a18027f9300
Maxime Stocklin <mstockli@c3r5s2.42lausanne.ch>
2023-09-05 16:49:54 +0200

    first instance of select()

--- Networking/Servers/TestServer.cpp
+++ Networking/Servers/TestServer.cpp
@@ -6,7 +6,7 @@
 /*   By: mstockli <mstockli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 15:29:07 by mstockli          #+#    #+#             */
-/*   Updated: 2023/09/05 15:10:11 by mstockli         ###   ########.fr       */
+/*   Updated: 2023/09/05 16:45:37 by mstockli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 #include <cstring>
 #include <iostream>
 
+#include <stdio.h> 
+#include <string.h>   //strlen 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <unistd.h>   //close 
+#include <arpa/inet.h>    //close 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
 
+	
+#define TRUE 1
+#define FALSE 0
 
-TestServer::TestServer() : Server(AF_INET, SOCK_STREAM, 0, 80, INADDR_ANY, 10)
+TestServer::TestServer() : Server(AF_INET, SOCK_STREAM, 0, 80, INADDR_ANY, 3)
 {
 	launch();
 }
@@ -29,7 +42,19 @@ void TestServer::accepter()
 
 	//get_socket() returns the main socket instance (ListenSocket class)
 	//get_sock() returns the main socket FD
-	new_socket = accept(get_socket()->get_sock(), (struct sockaddr *)&address, (socklen_t *)&addrlen);
+	/*new_socket = accept(get_socket()->get_sock(), (struct sockaddr *)&address, (socklen_t *)&addrlen);*/
+	
+	// added
+	if ((new_socket = accept(get_socket()->get_sock(), (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+	{
+		perror("Accept:");
+		exit(EXIT_FAILURE);
+	}
+
+	//inform user of socket number - used in send and receive commands
+	printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs
+		(address.sin_port));
+
 	
 	memset(buffer, 0, sizeof(buffer)); // Zero out the buffer
 	/*
@@ -46,9 +71,9 @@ void TestServer::accepter()
 // handle CGI scripts?
 void TestServer::handler()
 {
-	std::cout << "THIS IS THE BUFFER BIATCH" << std::endl;
+	std::cout << "HERE COMES THE BUFFER" << std::endl;
 	std::cout << buffer << std::endl;
-	std::cout << "THIS IS THE BUFFER BIATCH22" << std::endl;
+	std::cout << "THERE LEAVES THE BUFFER" << std::endl;
 }
 
 void TestServer::responder()
@@ -92,16 +117,115 @@ void TestServer::responder()
 
 void TestServer::launch()
 {
+	//set of socket descriptors
+	fd_set readfds;
+
+	int client_socket[30];
+	int max_clients = 30;
+	int max_sd;
+	int sd;
+	int activity;
+	int valread;
+	int i;
+
+	//initialise all client_socket[] to 0 so not checked
+	for (i = 0; i < max_clients; i++)
+	{
+		client_socket[i] = 0;
+	}
+
+	
 	int count = 0;
 	while (1)
 	{
+		//clear the socket set
+		FD_ZERO(&readfds);
+
+		//add master socket to set
+		FD_SET(get_socket()->get_sock(), &readfds);
+		max_sd = get_socket()->get_sock();
+
+		//add child sockets to set
+		for ( i = 0 ; i < max_clients ; i++)
+		{
+			//socket descriptor
+			sd = client_socket[i];
+				
+			//if valid socket descriptor then add to read list
+			if(sd > 0)
+				FD_SET( sd , &readfds);
+				
+			//highest file descriptor number, need it for the select function
+			if(sd > max_sd)
+				max_sd = sd;
+		}
 		std::cout << "============= WAITING =============" << std::endl;
-		accepter();
-		handler();
-		responder();
-		std::cout << "============= DONE =============" << std::endl;
-		std::cout << "count = " << count << std::endl;
-		count++;
+
+		//wait for an activity on one of the sockets , timeout is NULL ,
+		//so wait indefinitely
+		activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+	
+		if ((activity < 0) && (errno!=EINTR))
+		{
+			printf("select error");
+		}
+
+		//If something happened on the master socket ,
+		//then its an incoming connection
+		if (FD_ISSET(get_socket()->get_sock(), &readfds))
+		{
+			accepter();
+			sleep(2);
+			handler();
+			responder();
+			std::cout << "============= DONE =============" << std::endl;
+			std::cout << "count = " << count << std::endl;
+			count++;
+
+			//add new socket to array of sockets
+			for (i = 0; i < max_clients; i++)
+			{
+				//if position is empty
+				if( client_socket[i] == 0 )
+				{
+					client_socket[i] = new_socket;
+					printf("Adding to list of sockets as %d\n" , i);
+						
+					break;
+				}
+			}
+		}
+
+		for (i = 0; i < max_clients; i++)
+		{
+			sd = client_socket[i];
+				
+			if (FD_ISSET( sd , &readfds))
+			{
+				//Check if it was for closing , and also read the
+				//incoming message
+				if ((valread = read( sd , buffer, 1024)) == 0)
+				{
+					//Close the socket and mark as 0 in list for reuse
+					close( sd );
+					client_socket[i] = 0;
+				}
+					
+				//Echo back the message that came in
+				else
+				{
+					//set the string terminating NULL byte on the end
+					//of the data read
+					buffer[valread] = '\0';
+					send(sd , buffer , strlen(buffer) , 0 );
+				}
+			}
+		}
+
+
+
+
+		
 	}
 	// close(server_fd);
 }
